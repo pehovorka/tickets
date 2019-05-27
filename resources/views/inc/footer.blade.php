5b2b909bc043373d200c308ea0de4cdd87219fcb
Petr Hovorka <petr.hovorka@anywhere.cz>
2019-05-27 23:24:06 +0200

    Footer warning

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-muted mt-3 bg-light mb-0">
     <div class="container">
-        <p class="mb-0 pt-3">Semestrální práce z kurzu 4IZ278.</p>
+        <p class="mb-0 pt-3">Semestrální práce z kurzu <a href="http://4iz278.github.io/zakonceni/aplikace/">4IZ278</a>. Žádné z nabízených vstupenek nejsou platné!</p>
         <p class="mb-0">&copy; 2019 Petr Hovorka (hovp01)</p>
         <p class="mb-0 pb-3"><a href="/privacy-policy">Ochrana osobních údajů</a></p>
     </div>
